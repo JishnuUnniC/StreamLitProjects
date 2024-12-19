08376cc01f943b91e7a2339ee9119b331d736f34
jishnu-unni-c <jishnu.unni.c@hotmail.com>
2024-12-19 13:05:16 +0530

    Updated main with new model(pickle file) to predict flower classification

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import joblib
 import streamlit as st
+from os import path
+import numpy as np
 
-st.title("web app")
-x = st.slider("x")
-st.title("{} squared is {}".format(x, x*x))
+st.title("Flower Classification App")
+
+filename = "lr_model.pkl"
+lr_model = joblib.load(path.join("model", filename))
+
+sl = st.number_input("Insert a sepel length")
+sw = st.number_input("Insert a sepel width")
+pl = st.number_input("Insert a petal length")
+pw = st.number_input("Insert a petal width")
+
+if st.button("Predict"):
+    pred = lr_model.predict(np.array([[sl, sw, pl, pw]]))
+    st.write("The flower is :", pred[0])
